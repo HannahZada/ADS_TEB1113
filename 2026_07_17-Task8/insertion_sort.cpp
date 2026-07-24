19671a90d0028e115225ed1b56d13207dcbd4dfb
Farhana <hannanu12@gmail.com>
2026-07-24 17:44:47 +0800

    Implement bubble sort algorithm in insertion_sort.cpp

--- 2026_07_17-Task8/insertion_sort.cpp
+++ 2026_07_17-Task8/insertion_sort.cpp
@@ -1,1 +1,42 @@
 
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {45, 12, 89, 3, 67, 23, 91, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array (bubble sort):\n";
+    printArray(arr, n);
+
+    return 0;
+}
